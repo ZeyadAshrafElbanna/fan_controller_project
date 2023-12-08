cd6590a05c084ddb656def38126dcbbf6e9f7d37
ZeyadAshrafElbanna <zeyadashraf.alahly@gmail.com>
2023-12-08 19:15:08 +0200

    first commit

---
+++ Project_3/Project_3/Debug/source.d
@@ -0,0 +1,12 @@
+source.o source.o: ../source.c ../lcd.h ../std_types.h ../adc.h \
+  ../DC_Motor.h ../lm35_sensor.h
+
+../lcd.h:
+
+../std_types.h:
+
+../adc.h:
+
+../DC_Motor.h:
+
+../lm35_sensor.h:
---
+++ Project_3/Project_3/PWM.h
@@ -0,0 +1,12 @@
+/*
+ * PWM.h
+ *
+ *  Created on: 13 Oct 2023
+ *      Author: Zeyad El-banna
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+#include"std_types.h"
+void PWM_Timer0_Start(uint8 duty_cycle);
+#endif /* PWM_H_ */
